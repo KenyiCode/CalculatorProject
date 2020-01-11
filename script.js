981d5920be71552af8274c29fa2a538210e3b107
Kenyiro Anazagasty <kenyicode@gmail.com>
2020-01-10 23:02:22 -0500

    Rewriting and refactored code

--- script.js
+++ script.js
@@ -33,3 +33,42 @@ function operate(op, a, b){
             break;
     }
 }
+
+//Displays numbers clicked on screen
+document.getElementById('button_layout').addEventListener('click', function(e){
+    if (e.target && e.target.matches('.number')){
+        if (calcInput.value == "0" || opBool){
+            calcInput.value = e.target.textContent;
+            console.log(calcInput.value);
+            opBool = false;
+        }
+        else{
+            calcInput.value += e.target.textContent;
+        }
+    }
+});
+
+//Function for operator choice
+document.getElementById('button_layout').addEventListener('click', function(e){
+    if (e.target && e.target.matches('.operator')){
+        currentOp = e.target.textContent;
+        console.log(currentOp);
+        opBool = true;
+    }
+})
+
+let opBool = false;
+let tempValue = 0, operator = '';
+let btnValues = [0,1,2,3,4,5,6,7,8,9];
+let btnNumbers = document.getElementsByClassName("number");
+
+//Operator buttons
+let calcInput = document.getElementById("textField");
+let operators = document.getElementsByClassName('operator');
+let equals = document.getElementById('equal');
+let currentOp = "";
+
+//Event for clear button function
+let clearBtn = document.getElementById("clear").addEventListener('click', function(){
+    calcInput.value = '0';
+})
